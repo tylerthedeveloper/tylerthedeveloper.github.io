3dd91683bd21061169b423bafbdf492d74dae519
Tyler Citrin <tcitrin@indiana.edu>
2017-12-25 10:18:10 -0500

    finalized images, js, manifest

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,22 @@
-$(function(){
-	$('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
-});
-
-$('.Count').each(function () {
-  var $this = $(this);
-  $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-    duration: 6000,
-    easing: 'swing',
-    step: function () {
-      $this.text(Math.ceil(this.Counter));
-    }
+jQuery(function () {
+	"use strict";
+	//Slider 
+	$(document).ready(function(){
+    jQuery('.skillbar').each(function(){
+        jQuery(this).find('.skillbar-bar').animate({
+          width:jQuery(this).attr('data-percent')
+        },6000);
+      });
+    
+    jQuery('.Count').each(function () {
+      var $this = $(this);
+      $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+        duration: 6000,
+        easing: 'swing',
+        step: function () {
+          $this.text(Math.ceil(this.Counter));
+        }
+      });
+    });		
   });
 });
